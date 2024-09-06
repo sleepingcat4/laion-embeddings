56fdd9a074c5f75c2fb61a357a1183dcb044e5f3
endomorphosis <starworks5@gmail.com>
2024-09-05 19:41:03 -0700

    Refactor ipfs_embeddings_py and remove ipfs_multiformats.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from ipfs_embeddings_py.ipfs_embeddings import ipfs_embeddings_py
+from ipfs_embeddings_py import ipfs_embeddings_py
 from datasets import load_dataset
 
 class test_ipfs_embeddings:
