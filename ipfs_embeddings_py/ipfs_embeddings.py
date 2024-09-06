36fcd74856fbf4df5bbf91eb8a6d07d9f00ddd5e
endomorphosis <starworks5@gmail.com>
2024-09-05 17:28:53 -0700

    feat: Add endpoint status tracking to ipfs_embeddings_py
    The code changes add a new attribute `endpointStatus` to the `ipfs_embeddings_py` class. This attribute is used to track the status of the TEI HTTPS and libp2p endpoints. The `add_tei_https_endpoint` and `add_libp2p_endpoint` methods have been updated to store the batch size along with the endpoint. The `rm_tei_https_endpoint` and `rm_libp2p_endpoint` methods have been updated to remove the endpoint from the `endpointStatus` attribute.

--- ipfs_embeddings_py/ipfs_embeddings.py
+++ ipfs_embeddings_py/ipfs_embeddings.py
@@ -1,13 +1,13 @@
 
 class ipfs_embeddings_py:
-    def __init__(self, ipfs_path):
-        self.ipfs_path = ipfs_path
+    def __init__(self, resources, metedata):
         self.tei_https_endpoints = {}
         self.libp2p_endpoints = {}
         self.queue = iter([])
         self.index = 0
         self.index = {}
         self.ipfsIndex = {}
+        self.endpointStatus = {}
         return None
     
     def load_index(self, index):
@@ -16,26 +16,26 @@ class ipfs_embeddings_py:
     
     def add_tei_https_endpoint(self, model, endpoint, batch_size):
         if model not in self.tei_https_endpoints:
-            self.tei_https_endpoints[model] = []
+            self.tei_https_endpoints[model] = {}
         if endpoint not in self.tei_https_endpoints[model]:  
-            self.tei_https_endpoints[model].push({endpoint: batch_size})
+            self.tei_https_endpoints[model][endpoint] = batch_size
         return None
     
     def add_libp2p_endpoint(self, model, endpoint, batch_size):
         if model not in self.libp2p_endpoints:
-            self.libp2p_endpoints[model] = []
+            self.libp2p_endpoints[model] = {}
         if endpoint not in self.libp2p_endpoints[model]:  
-            self.libp2p_endpoints[model].push({endpoint: batch_size})
+            self.libp2p_endpoints[model][endpoint] = batch_size
         return None
     
     def rm_tei_https_endpoint(self, model, endpoint):
         if model in self.tei_https_endpoints and endpoint in self.tei_https_endpoints[model]:
-            self.tei_https_endpoints[model].remove(endpoint in self.libp2p_endpoints[model].keys().filter(endpoint))
+            del self.tei_https_endpoints[model][endpoint]
         return None
     
     def rm_libp2p_endpoint(self, model, endpoint):
         if model in self.libp2p_endpoints and endpoint in self.libp2p_endpoints[model]:
-            self.libp2p_endpoints[model].remove(endpoint in self.libp2p_endpoints[model].keys().filter(endpoint))
+            del self.libp2p_endpoints[model][endpoint]
         return None
     
     def test_tei_https_endpoint(self, model, endpoint):
@@ -53,6 +53,8 @@ class ipfs_embeddings_py:
         self.add_tei_https_endpoint("BAAI/bge-m3", "62.146.169.111:8080/embed",1)
         self.add_tei_https_endpoint("BAAI/bge-m3", "62.146.168.111:8081/embed",1)
         print("test")
+
+    def status 
 
 if __name__ == '__main__':
     ipfs_embeddings = ipfs_embeddings_py()
