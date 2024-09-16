da22a8dfb9b4b932968b171b52af2418fc5294fd
endomorphosis <starworks5@gmail.com>
2024-09-16 02:52:45 -0700

    Refactor create_embeddings_batch.py to optimize code efficiency and remove unnecessary imports

--- create_embeddings/create_embeddings.py
+++ create_embeddings/create_embeddings.py
@@ -1,23 +1,22 @@
+import asyncio
+from aiohttp import ClientSession
+from datasets import load_dataset
 import os
 import sys
 import datasets
-import json
 from ipfs_embeddings_py import ipfs_embeddings_py
-import pandas as pd
-from qdrant_client import QdrantClient
-from qdrant_client.http import models
-from qdrant_client.models import Distance, VectorParams
-import numpy as np
 import os
 import sys
 import subprocess
-import json
+from transformers import AutoTokenizer
 
-class create_embeddings:
+class create_embeddings_batch:
     def __init__(self, resources, metadata):
         self.resources = resources
         self.metadata = metadata
         self.datasets = datasets
+        self.index =  {}
+        self.cid_list = []
         if len(list(metadata.keys())) > 0:
             for key in metadata.keys():
                 setattr(self, key, metadata[key])
@@ -26,79 +25,59 @@ class create_embeddings:
             for endpoint in resources["https_endpoints"]:
                 self.ipfs_embeddings_py.add_https_endpoint(endpoint[0], endpoint[1], endpoint[2])
         else:
-            self.ipfs_embeddings_py.add_https_endpoint("BAAI/bge-m3", "http://62.146.169.111:80/embed",1)
+            self.ipfs_embeddings_py.add_https_endpoint("BAAI/bge-m3", "http://62.146.169.111:80/embed", 8192)
+            self.ipfs_embeddings_py.add_https_endpoint("Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://62.146.169.111:80/embed", 32768 )
         self.join_column = None
+        self.tokenizer = {}
 
-    def index_dataset (self, dataset, faiss_dst, model):
-        #check if the dataset exists in the faiss index
-        dataset_exists = False
-        try:
-            self.faiss_index = self.datasets.load_dataset(faiss_dst)
-        except:
-            self.faiss_index = datasets.Dataset.from_dict({"cid": [], "embedding": []})
-        self.dataset = self.datasets.load_dataset(dataset)['train']
-        self.dataset_name = dataset
-        self.faiss_index_name = faiss_dst
-        dataset_columns = self.dataset.column_names
-        dataset_columns.append("cid")
-        ## create new column in the dataset called "cid"
-        self.dataset.add_column("cid", [str(i) for i in range(len(self.dataset))])
-        self.new_dataset = datasets.Dataset.from_dict({key: [] for key in dataset_columns })
-        new_rows = []
-        for row in self.dataset:
-            this_row = row
-            cid = self.ipfs_embeddings_py.index_cid(this_row["text"])
-            this_row["cid"] = cid
-            find_cid = self.faiss_index.filter(lambda x: x["cid"] == cid)
-            if find_cid.num_rows > 0:
-                pass
+    async def main(self, dataset, column, dst_path, models):
+        if not os.path.exists(dst_path):
+            os.makedirs(dst_path)
+        self.ipfs_embeddings_py.queues = {}
+        consumer_tasks = {}
+        batch_sizes = {}
+        self.dataset = load_dataset(dataset, split='train', streaming=True)
+        columns = self.dataset.column_names
+        columns.append("cid")
+        if os.path.isfile(f"{dst_path}/{dataset}.arrow") == True:
+            self.new_dataset = self.datasets.load_dataset(f"{dst_path}/{dataset}.arrow")
+            self.cid_list = self.new_dataset["cid"]
+        else:
+            self.ipfs_embeddings_py.new_dataset = datasets.Dataset.from_dict({key: [] for key in columns })
+
+        for model in models:
+            batch_size = 32
+            if os.path.isfile(f"{dst_path}/{model.replace("/","---")}.arrow") == True:
+                self.index[model] = self.datasets.load_dataset(f"{dst_path}/{model.replace("/","---")}.arrow")
             else:
-                embedding = self.ipfs_embeddings_py.index_knn(this_row["text"], model)[0]
-                new_row = {}
-                new_row["cid"] = cid
-                new_row["embedding"] = embedding
-                new_rows.append(new_row)
-        new_dataset = datasets.Dataset.from_dict({key: [row[key] for row in new_rows] for key in new_rows[0].keys()})
-        self.faiss_index = new_dataset
-        self.dataset.save_to_disk(f"/storage/teraflopai/{self.dataset_name}.arrow")
-        self.faiss_index.save_to_disk(f"/storage/teraflopai/{self.faiss_index_name}.arrow")
-        self.dataset.to_parquet(f"/storage/teraflopai/{self.dataset_name}.parquet")
-        self.faiss_index.to_parquet(f"/storage/teraflopai/{self.faiss_index_name}.parquet")
-        self.dataset.push_to_hub(self.dataset_name, use_temp_dir=True, message="Update dataset")
-        self.faiss_index.push_to_hub(self.faiss_index_name, use_temp_dir=True, message="Update faiss index")
+                self.index[model] = datasets.Dataset.from_dict({"cid": [], "embedding": []})
+                self.ipfs_embeddings_py.queues[model] = asyncio.Queue(batch_size)
+                consumer_tasks[model] = asyncio.create_task(self.ipfs_embeddings_py.consumer(self.ipfs_embeddings_py.queues[model], column, batch_size, model))
+
+        producer_task = asyncio.create_task(self.ipfs_embeddings_py.producer(self.dataset, column, self.ipfs_embeddings_py.queues))        
+        save_task = asyncio.create_task(self.ipfs_embeddings_py.save_to_disk(dataset, dst_path, models))
+        await asyncio.gather(producer_task, save_task, *consumer_tasks.values()) 
         return None
     
-    async def batch_index_dataset (self, dataset, faiss_dst, model):
-        dataset_exists = False
-        try:
-            self.faiss_index = self.datasets.load_dataset(faiss_dst)
-        except:
-            self.faiss_index = datasets.Dataset.from_dict({"cid": [], "embedding": []})
-        self.dataset = self.datasets.load_dataset(dataset)['train']
-        self.dataset_name = dataset
-        self.faiss_index_name = faiss_dst
-        dataset_columns = self.dataset.column_names
-        dataset_columns.append("cid")
-        ## create new column in the dataset called "cid"
-        self.dataset.add_column("cid", [str(i) for i in range(len(self.dataset))])
-        self.new_dataset = datasets.Dataset.from_dict({key: [] for key in dataset_columns })
-        new_rows = []
-        for this_row in self.dataset:
-            self.ipfs_embeddings_py.queue_index_cid(this_row["text"])
-            self.ipfs_embeddings_py.queue_index_knn(this_row["text"], model)
-        return None
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     metadata = {
         "dataset": "TeraflopAI/Caselaw_Access_Project",
-        "faiss_index": "endomorphosis/Caselaw_Access_Project_M3_Embeddings",
-        "model": "BAAI/bge-m3"
+        "column": "text",
+        "models": [
+            "BAAI/bge-m3",
+            "Alibaba-NLP/gte-Qwen2-1.5B-instruct",
+            # "dunzhang/stella_en_1.5B_v5",
+        ],
+        "dst_path": "/storage/teraflopai"
     }
     resources = {
-        "https_endpoints": [["BAAI/bge-m3", "http://62.146.169.111:80/embed",1]]
+        "https_endpoints": [
+            ["BAAI/bge-m3", "http://62.146.169.111:80/embed", 8191],
+            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://127.0.0.1:8080/embed", 32786],
+            ["dunzhang/stella_en_1.5B_v5", "http://127.0.0.1:8080/embed", 131072]
+        ]
     }
-    create_embeddings = create_embeddings(resources, metadata)
-    # results = create_embeddings.index_dataset(metadata["dataset"], metadata["faiss_index"], metadata["model"])
-    results = create_embeddings.index_dataset(metadata["dataset"], metadata["faiss_index"], metadata["model"])
+    create_embeddings_batch = create_embeddings_batch(resources, metadata)
+    asyncio.run(create_embeddings_batch.main(metadata["dataset"], metadata["column"], metadata["dst_path"], metadata["models"]))    
 
-    print(results)
+
