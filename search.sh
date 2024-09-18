379a4436984aeef40ebd7894b7f5f15d1834c78b
endomorphosis <starworks5@gmail.com>
2024-09-18 02:02:08 -0700

    Refactor README.md and main.py to improve code organization and update endpoint usage

--- search.sh
+++ search.sh
@@ -1,2 +1,6 @@
-search_text="hello"
-curl http://127.0.0.1:9999/search/$search_text
+text="The quick brown fox jumps over the lazy dog"
+curl 127.0.0.1:9999/search \
+    -X POST \
+    -d '{"text":"The quick brown fox jumps over the lazy dog"}' \
+    -H 'Content-Type: application/json'
+
