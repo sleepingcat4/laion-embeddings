4f5ce5acbb0ef4a6835c9b2b029e109b7f13d75d
endomorphosis <starworks5@gmail.com>
2024-09-19 23:56:55 -0700

    Refactor create.sh to update endpoint port number

--- create.sh
+++ create.sh
@@ -9,7 +9,7 @@ metadata='{
 }'
 resources='{
     "https_endpoints": [
-        ["BAAI/bge-m3", "http://127.0.0.1:8080/embed", 8190],
+        ["BAAI/bge-m3", "http://127.0.0.1:8080/embed", 8188],
         ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://127.0.0.1:8081/embed", 32766],
         ["dunzhang/stella_en_1.5B_v5", "http://127.0.0.1:8082/embed", 131072]
     ]
--- create_embeddings/create_embeddings.py
+++ create_embeddings/create_embeddings.py
@@ -81,8 +81,8 @@ if __name__ == "__main__":
     }
     resources = {
         "https_endpoints": [
-            ["BAAI/bge-m3", "http://127.0.0.1:8080/embed", 8190],
-            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://127.0.0.1:8081/embed", 32766],
+            ["BAAI/bge-m3", "http://127.0.0.1:8080/embed", 8188],
+            ["Alibaba-NLP/gte-Qwen2-1.5B-instruct", "http://127.0.0.1:8081/embed", 32764],
             ["dunzhang/stella_en_1.5B_v5", "http://127.0.0.1:8082/embed", 131072]
         ]
     }
