ef06505d19e6c447f8875b8cc15564ee95285aba
endomorphosis <starworks5@gmail.com>
2024-09-20 13:38:06 -0700

    Refactor Dockerfile to add new model and update GPU settings

--- launch_tei.sh
+++ launch_tei.sh
@@ -6,3 +6,6 @@ docker run --gpus all -e CUDA_VISIBLE_DEVICES=1  -p 8081:80 -v $volume:/data --p
 model=Alibaba-NLP/gte-Qwen2-1.5B-instruct
 docker run --gpus all -e CUDA_VISIBLE_DEVICES=0  -p 8082:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model --max-batch-tokens 32768
 docker run --gpus all -e CUDA_VISIBLE_DEVICES=1  -p 8083:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model --max-batch-tokens 32768
+model=dunzhang/stella_en_1.5B_v5
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=0  -p 8082:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model --max-batch-tokens 32768
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=1  -p 8083:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model --max-batch-tokens 32768
