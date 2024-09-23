cdeb206c343e97afe660f5aeab255582243b9294
endomorphosis <starworks5@gmail.com>
2024-09-23 04:32:15 -0700

    Refactor search.sh to fix syntax error in collection parameter

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 text="orange juice"
 curl 127.0.0.1:9999/search \
     -X POST \
-    -d '{"text":"orange juice", "collection: "Wikipedia-X-Concat"}' \
+    -d '{"text":"orange juice", "collection": "Wikipedia-X-Concat"}' \
     -H 'Content-Type: application/json'
 
